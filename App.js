535879591387ba74b65861774bee27ce114daacf
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-21 16:27:55 +0100

    Added functionality in App to fetch fonts

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
+import {AppLoading} from "expo";
+
+const fetchFonts = () => {
+ return Font.loadAsync({
+   OpenSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+   OpenSansBold: require('./assets/fonts/OpenSans-Bold.ttf'),
+ });
+};
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setLoading(false)}
+        onError={error => console.log(error)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
