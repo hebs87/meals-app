87f75e45056bd9b39150be969f2c95daf1dffb2d
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-23 22:25:36 +0100

    Added button in MealDetailsScreen to go back to home screen on press

--- screens/MealDetailsScreen/MealDetailsScreen.js
+++ screens/MealDetailsScreen/MealDetailsScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 const MealDetailsScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>Meal Details Screen!</Text>
+      <Button
+        title='Back to Categories!'
+        onPress={() => props.navigation.popToTop()}
+      />
     </View>
   );
 };
