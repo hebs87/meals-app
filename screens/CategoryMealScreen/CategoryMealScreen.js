51de1145b50f5802f59b7c9363439a1bfcc5112b
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-23 22:09:57 +0100

    Added button in CategoryMealsScreen to navigate to MealDetailsScreen

--- screens/CategoryMealScreen/CategoryMealScreen.js
+++ screens/CategoryMealScreen/CategoryMealScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 const CategoryMealScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>Category Meal Screen!</Text>
+      <Button
+        title='Go to Meal!'
+        onPress={() => props.navigation.navigate('MealDetails')}
+      />
     </View>
   );
 };
