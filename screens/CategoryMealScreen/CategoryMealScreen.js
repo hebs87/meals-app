b12be8356357e2437578ef0bcf78368e96c9fc79
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-24 21:15:43 +0100

    Removed unused imports from project

--- screens/CategoryMealScreen/CategoryMealScreen.js
+++ screens/CategoryMealScreen/CategoryMealScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, FlatList} from 'react-native';
 import MealItem from "../../components/MealItem/MealItem";
 import {MEALS} from "../../data/dummy-data";
 
