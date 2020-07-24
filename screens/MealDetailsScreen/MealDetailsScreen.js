4c191e1a15d2d5c7df01d179646177eb298c8473
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-24 21:17:54 +0100

    Passed mealId param into MealDetailsScreen and extracted and rendered correct meal

--- screens/MealDetailsScreen/MealDetailsScreen.js
+++ screens/MealDetailsScreen/MealDetailsScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
+import {MEALS} from "../../data/dummy-data";
 
 const MealDetailsScreen = props => {
+  const mealId = props.navigation.getParam('mealId');
+
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
   return (
     <View style={styles.screen}>
       <Text>Meal Details Screen!</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
         title='Back to Categories!'
         onPress={() => props.navigation.popToTop()}
