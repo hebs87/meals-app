c920c9bd93d8422ca530388a7cf3639009eb2dd4
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-24 20:13:13 +0100

    Rendered meal data in CategoryMealScreen for meals matching categoryIds

--- screens/CategoryMealScreen/CategoryMealScreen.js
+++ screens/CategoryMealScreen/CategoryMealScreen.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
-import {CATEGORIES} from "../../data/dummy-data";
+import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native';
+import {MEALS} from "../../data/dummy-data";
 
 const CategoryMealScreen = props => {
   const catId = props.navigation.getParam('categoryId');
-  const selectedCat = CATEGORIES.find(cat => cat.id === catId);
+  // Get the meals if the catId is in the categoryIds array of the Meal object
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  const renderMealItem = itemData => {
+    return (
+      <View>
+        <Text>
+          {itemData.item.title}
+        </Text>
+      </View>
+    );
+  };
 
   return (
     <View style={styles.screen}>
-      <Text>Category Meal Screen!</Text>
-      <Text>{selectedCat.title}</Text>
-      <Button
-        title='Go to Meal!'
-        onPress={() => props.navigation.navigate('MealDetails')}
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={displayedMeals}
+        renderItem={renderMealItem}
       />
     </View>
   );
