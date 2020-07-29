c71a360d2c790ff145e899808cc1ba5c54729b9e
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-29 13:43:47 +0100

    Added logic in reducer to remove/add meals from favourites list

--- store/reducers/meals.reducers.js
+++ store/reducers/meals.reducers.js
@@ -1,4 +1,5 @@
 import {MEALS} from "../../data/dummy-data";
+import {TOGGLE_FAVOURITE} from "../constants/constants";
 
 const initialState = {
   meals: MEALS,
@@ -7,7 +8,28 @@ const initialState = {
 };
 
 const mealsReducer = (state=initialState, action) => {
-  return state;
+  switch (action.type) {
+    case TOGGLE_FAVOURITE:
+      // Get the index of the meal if it exists in favourites - return -1 if not
+      const existingIndex = state.favouriteMeals.findIndex(meal => meal.id === action.payload);
+      if (existingIndex >= 0) {
+        // Remove if already exists - create new array of favourite meals so we don't manipulate the existing one
+        // Then remove the selected meal from the list
+        const updatedFavMeals = [...state.favouriteMeals];
+        updatedFavMeals.splice(existingIndex, 1);
+        return {...state, favouriteMeals: updatedFavMeals};
+        // Alternative syntax
+        // return Object.assign({}, state, {favouriteMeals: updatedFavMeals});
+      } else {
+        // Add if doesn't exist
+        const meal = state.meals.find(meal => meal.id === action.payload);
+        return {...state, favouriteMeals: state.favouriteMeals.concat(meal)};
+        // Alternative syntax
+        // return Object.assign({}, state, {favouriteMeals: [...state.favouriteMeals, meal]});
+      }
+    default:
+      return state;
+  }
 };
 
 export default mealsReducer;
