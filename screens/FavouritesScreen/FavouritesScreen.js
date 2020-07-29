cb23bbb96e8aa0577c8c5acd4bc1c92ecb44e311
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-29 16:27:33 +0100

    Added fallback text in FavouritesScreen if no favourite meals present

--- screens/FavouritesScreen/FavouritesScreen.js
+++ screens/FavouritesScreen/FavouritesScreen.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {useSelector} from "react-redux";
+import {StyleSheet, View} from 'react-native';
+import DefaultText from "../../components/DefaultText/DefaultText";
 import MealsList from "../../components/MealsList/MealsList";
 
 const FavouritesScreen = props => {
   // Get favourite meals from the state
   const favMeals = useSelector(state => state.meals.favouriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favourite meals found. Start adding some!</DefaultText>
+      </View>
+    )
+  }
 
   return (
     <MealsList
@@ -13,5 +23,13 @@ const FavouritesScreen = props => {
     />
   );
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default FavouritesScreen;
